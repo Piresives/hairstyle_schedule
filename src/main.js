e8e46ec51f1a824cf6adf23ead370cee35831ca9
Piresives <ivespmiranda@hotmail.com>
2024-10-18 20:25:52 -0300

    Iniciando projeto de agendamento

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//Config do dayjs
+import "./libs/day.js"
+
+//CSS
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
+// import dayjs from "dayjs"
+// console.log(dayjs().format("DD/MM HH:mm"))
